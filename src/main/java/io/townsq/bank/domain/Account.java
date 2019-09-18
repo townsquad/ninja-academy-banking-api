b4120e9427948f3392ec705fbbde58a7af885f12
Matheus Santos <matheussantos@WindowsXP.local>
2019-09-17 21:26:43 -0300

    add account and repository

---
+++ src/main/java/io/townsq/bank/domain/Account.java
@@ -0,0 +1,38 @@
+package io.townsq.bank.domain;
+
+public class Account {
+    private String accountNumber;
+    private String owner;
+    private double available;
+
+
+    public Account(String accountNumber, String owner, double available) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.available = available;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(double available) {
+        this.available = available;
+    }
+}
---
+++ src/main/java/io/townsq/bank/repository/AccountRepository.java
@@ -0,0 +1,31 @@
+package io.townsq.bank.repository;
+
+import static java.util.Arrays.asList;
+
+import io.townsq.bank.domain.Account;
+import java.util.List;
+
+public class AccountRepository {
+    private static List<Account> accounts = asList(
+            new Account("00001-6", "Joao", 1235),
+            new Account("00002-6", "Maria", 467235),
+            new Account("00003-6", "Alberto", 98709123),
+            new Account("00004-6", "Mariana", 345),
+            new Account("00005-6", "Paloma", 5674),
+            new Account("10001-6", "Reginaldo", 67634),
+            new Account("10002-6", "Valdisnei", 437865)
+            );
+
+    public Account get(String accountNumber) {
+        return accounts
+                .stream()
+                .filter(a-> a.getAccountNumber().equals(accountNumber))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Account add(Account account) {
+        accounts.add(account);
+        return account;
+    }
+}
