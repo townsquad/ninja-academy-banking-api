ec83fc076bdd0aba196119a9814310daecd0a1a4
Matheus Santos <matheussantos@WindowsXP.local>
2019-09-24 17:01:18 -0300

    add get account

---
+++ src/main/java/io/townsq/bank/controller/AccountsController.java
@@ -0,0 +1,35 @@
+package io.townsq.bank.controller;
+
+import static org.springframework.http.ResponseEntity.noContent;
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.ok;
+
+import io.townsq.bank.domain.Account;
+import io.townsq.bank.repository.AccountRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/Accounts")
+public class AccountsController {
+
+    private final AccountRepository repo = new AccountRepository();
+    @GetMapping
+    public Account get() {
+
+        return new Account("123", "joao", 100);
+    }
+
+    @GetMapping("/{accountNumber}")
+    public ResponseEntity<Account> get(@PathVariable String accountNumber) {
+        Account account =  repo.get(accountNumber);
+        if(account == null){
+            return notFound().build();
+        }
+
+        return ok(account);
+    }
+}
