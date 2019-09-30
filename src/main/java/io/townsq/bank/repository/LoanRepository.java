e1b64745fd4fc32d2715cc7acaef5247f57b18ab
Bruno <bruno@townsq.com.br>
2019-09-30 17:30:26 -0300

    created method to save loan on repo

--- src/main/java/io/townsq/bank/repository/LoanRepository.java
+++ src/main/java/io/townsq/bank/repository/LoanRepository.java
@@ -5,17 +5,24 @@ import static java.util.Arrays.asList;
 
 import io.townsq.bank.domain.Loan;
 import io.townsq.bank.domain.Status;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LoanRepository {
-
-    private static List<Loan> loans = asList(
-            new Loan("00001-6", 1212, Status.APPROVED),
-            new Loan("00002-6", 10000, Status.WAITING),
-            new Loan("00003-6", 10000000, Status.DENIED)
-    );
+    private static List<Loan> loans;
+    public LoanRepository() {
+        loans = new ArrayList<>();
+        loans.add(new Loan("00001-6", 1212, Status.APPROVED));
+        loans.add(new Loan("00002-6", 10000, Status.WAITING));
+        loans.add(new Loan("00003-6", 10000000, Status.DENIED));
+    }
 
     public List<Loan> getAll() {
         return loans;
+    }
+
+    public Loan save(Loan loan) {
+        loans.add(loan);
+        return loan;
     }
 }
