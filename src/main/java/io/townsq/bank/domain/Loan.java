30d9eb0f75dd02286951573729d7e38577a37acd
Bruno <bruno@townsq.com.br>
2019-09-30 17:12:15 -0300

    created Loan constructor

--- src/main/java/io/townsq/bank/domain/Loan.java
+++ src/main/java/io/townsq/bank/domain/Loan.java
@@ -1,7 +1,14 @@
 package io.townsq.bank.domain;
 
 public class Loan {
-    String accountNumber;
-    double amount;
-    Status status;
+
+    public Loan(String accountNumber, double amount, Status status) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    private String accountNumber;
+    private double amount;
+    private Status status;
 }
