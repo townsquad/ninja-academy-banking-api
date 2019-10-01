afd93f814ab42c217fd0a66932e6654ba8963e61
Bruno <bruno@townsq.com.br>
2019-10-01 16:25:48 -0300

    Removed unused imports

--- src/main/java/io/townsq/bank/repository/LoanRepository.java
+++ src/main/java/io/townsq/bank/repository/LoanRepository.java
@@ -1,8 +1,5 @@
 package io.townsq.bank.repository;
 
-import static java.util.Arrays.asList;
-
-
 import io.townsq.bank.domain.Loan;
 import io.townsq.bank.domain.Status;
 import java.util.ArrayList;
@@ -10,10 +7,11 @@ import java.util.List;
 
 public class LoanRepository {
     private static List<Loan> loans;
+
     public LoanRepository() {
         loans = new ArrayList<>();
         loans.add(new Loan("123", "00001-6", 1212, Status.APPROVED));
-        loans.add(new Loan("345","00002-6", 10000, Status.WAITING));
+        loans.add(new Loan("345", "00002-6", 10000, Status.WAITING));
         loans.add(new Loan("678", "00003-6", 10000000, Status.DENIED));
     }
 
